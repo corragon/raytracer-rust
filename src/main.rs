8888c7a3e739957d1db6e833751b2011385ba578
Thomas Y <corragon@gmail.com>
2017-04-05 22:05:17 -0500

    Add really bad first version of vectors

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 use std::io::{Write, BufWriter};
 use std::fs::File;
+mod Vector;
 
 fn main() {
-  let nx = 300;
+  let V = Vector::Vector::vec3 { c1: 200f64, c2: 200f64, c3: 255f64 };
+  let nx = 200;
   let ny = 200;
-  let data = format!("{}\n{} {}\n255\n", "P3", nx, ny);
+  let data = format!("{}\n{} {}\n{}\n", "P3", V.c1, V.c2, V.c3);
   let f = File::create("target/image.ppm").expect("Unable to create file");
   let mut f = BufWriter::new(f);
   f.write_all(data.as_bytes()).expect("Unable to write data");
-  
+
   for j in (0..ny).rev() {
     for i in 0..nx {
       let r : f32 = i as f32 / nx as f32;
       let g : f32 = j as f32 / ny as f32;
-      let b : f32 = 0.2;
+      let b : f32 = 0.5;
       let ir = (255.99*r) as i32;
       let ig = (255.99*g) as i32;
       let ib = (255.99*b) as i32;
