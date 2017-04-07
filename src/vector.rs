e78f8cb3aa012471fc514555f6c7d097665f18d2
Thomas Y <corragon@gmail.com>
2017-04-06 23:25:54 -0500

    Implement traits for vector addition and subtraction

--- src/vector.rs
+++ src/vector.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, Sub};
+
 pub struct Vec3 {
   pub i : f64,
   pub j : f64,
@@ -7,5 +9,19 @@ pub struct Vec3 {
 impl Vec3 {
   pub fn length(&self) -> f64 {
     (self.i * self.i + self.j * self.j + self.k * self.k).sqrt()
+  }
+}
+
+impl Add for Vec3 {
+  type Output = Vec3;
+  fn add(self, other: Vec3) -> Vec3 {
+      Vec3 {i: self.i + other.i, j: self.j + other.j, k: self.k + other.k}
+  }
+}
+
+impl Sub for Vec3 {
+  type Output = Vec3;
+  fn sub(self, other: Vec3) -> Vec3 {
+      Vec3 {i: self.i - other.i, j: self.j - other.j, k: self.k - other.k}
   }
 }
