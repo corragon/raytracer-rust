2a1ad94c7c057cfd5339cb7c95a0866e5fab614f
Thomas Y <corragon@gmail.com>
2017-04-06 22:33:22 -0500

    Adjust casing and remove unnecessary mod declaration

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::io::{Write, BufWriter};
 use std::fs::File;
-mod Vector;
+mod vector;
 
 fn main() {
-  let V = Vector::Vector::vec3 { c1: 200f64, c2: 200f64, c3: 255f64 };
+  let v = vector::Vec3 { c1: 200f64, c2: 200f64, c3: 255f64 };
   let nx = 200;
   let ny = 200;
-  let data = format!("{}\n{} {}\n{}\n", "P3", V.c1, V.c2, V.c3);
+  let data = format!("{}\n{} {}\n{}\n", "P3", v.c1, v.c2, v.c3);
   let f = File::create("target/image.ppm").expect("Unable to create file");
   let mut f = BufWriter::new(f);
   f.write_all(data.as_bytes()).expect("Unable to write data");
@@ -15,7 +15,7 @@ fn main() {
     for i in 0..nx {
       let r : f32 = i as f32 / nx as f32;
       let g : f32 = j as f32 / ny as f32;
-      let b : f32 = 0.5;
+      let b : f32 = 0.7;
       let ir = (255.99*r) as i32;
       let ig = (255.99*g) as i32;
       let ib = (255.99*b) as i32;
--- src/vector.rs
+++ src/vector.rs
@@ -1,14 +1,11 @@
-pub mod Vector {
-
-  pub struct vec3 {
+  pub struct Vec3 {
     pub c1 : f64,
     pub c2 : f64,
     pub c3 : f64,
   }
 
-  impl vec3 {
+  impl Vec3 {
     pub fn length(&self) -> f64 {
       (self.c1 * self.c1 + self.c2 * self.c2 + self.c3 * self.c3).sqrt()
     }
   }
-}
