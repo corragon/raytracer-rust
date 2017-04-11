4dd5571b9fd437573cb470dc71270e35ccdd2974
Thomas Y <corragon@gmail.com>
2017-04-10 22:19:52 -0500

    Use ray for calculating blue-white blend

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,35 @@ use vector::Vec3;
 mod ray;
 use ray::Ray;
 
+fn color(ray : Ray) -> Vec3 {
+  let unit_direction = ray.direction().unit_vector();
+  let t = 0.5 * (unit_direction.y() + 1.0);
+  return Vec3::new(1.0,1.0,1.0) * (1.0 - t) + Vec3::new(0.5,0.7,1.0) * t;
+}
+
 fn main() {
-  let nx = 300;
-  let ny = 200;
+  let nx = 200;
+  let ny = 100;
   let data = format!("{}\n{} {}\n{}\n", "P3", nx, ny, 255);
   let f = File::create("target/image.ppm").expect("Unable to create file");
   let mut f = BufWriter::new(f);
   f.write_all(data.as_bytes()).expect("Unable to write data");
 
+  let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+  let horizontal = Vec3::new(4.0, 0.0, 0.0);
+  let vertical = Vec3::new(0.0, 2.0, 0.0);
+  let origin = Vec3::new(0.0, 0.0, 0.0);
+
   for j in (0..ny).rev() {
     for i in 0..nx {
-      let color = Vec3::new(i as f64 / nx as f64, j as f64 / ny as f64, 0.5);
+      let u = i as f64 / nx as f64;
+      let v = j as f64 / ny as f64;
+      let r = Ray::new(origin, lower_left_corner + horizontal * u + vertical * v);
+      let col = color(r);
 
-      let ir = (255.99*color.i) as i32;
-      let ig = (255.99*color.j) as i32;
-      let ib = (255.99*color.k) as i32;
+      let ir = (255.99*col.r()) as i32;
+      let ig = (255.99*col.g()) as i32;
+      let ib = (255.99*col.b()) as i32;
       f.write_all(format!("{} {} {}\n", ir, ig, ib).as_bytes()).expect("Unable to write data");
     }
   }
