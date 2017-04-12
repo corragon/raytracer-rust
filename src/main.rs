35ff8531a12772da838d35f62900c22def275309
Thomas Y <corragon@gmail.com>
2017-04-11 22:33:01 -0500

    Add basic check for a sphere

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use std::io::{Write, BufWriter};
 use std::fs::File;
 mod vector;
-use vector::Vec3;
+use vector::{Vec3, dot, cross, unit_vector};
 mod ray;
 use ray::Ray;
 
+fn hit_sphere(center: Vec3, radius: f64, ray: Ray) -> bool {
+  let oc = ray.origin() - center;
+  let a: f64 = dot(ray.direction(), ray.direction());
+  let b: f64 = dot(oc, ray.direction()) * 2.0;
+  let c: f64 = dot(oc, oc) - radius * radius;
+  let discriminant = b * b - 4.0 * a * c;
+  return discriminant > 0.0
+}
+
 fn color(ray : Ray) -> Vec3 {
+  if hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, ray) {
+    return Vec3::new(1.0, 0.0, 0.0);
+  }
   let unit_direction = ray.direction().unit_vector();
   let t = 0.5 * (unit_direction.y() + 1.0);
   return Vec3::new(1.0,1.0,1.0) * (1.0 - t) + Vec3::new(0.5,0.7,1.0) * t;
