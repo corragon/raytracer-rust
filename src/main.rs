e4c87e024ce4a2110497102aeaadeff94bd42fa8
Thomas Y <corragon@gmail.com>
2017-04-15 23:56:41 -0500

    Add antialiasing

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ use ray::Ray;
 use sphere::{Sphere};
 use hitable::{Hitable, hit_record};
 use hitable_list::Hitable_list;
+use camera::Camera;
 
 
 fn color<T: Hitable>(ray : Ray, world : &T) -> Vec3 {
@@ -33,6 +34,7 @@ fn color<T: Hitable>(ray : Ray, world : &T) -> Vec3 {
 fn main() {
   let nx = 200;
   let ny = 100;
+  let ns = 100;
   let data = format!("{}\n{} {}\n{}\n", "P3", nx, ny, 255);
   let f = File::create("target/image.ppm").expect("Unable to create file");
   let mut f = BufWriter::new(f);
@@ -43,20 +45,28 @@ fn main() {
   let vertical = Vec3::new(0.0, 2.0, 0.0);
   let origin = Vec3::new(0.0, 0.0, 0.0);
 
+  let cam = Camera::new(lower_left_corner, vertical, horizontal, origin);
+
   let mut list : Vec<Box<Hitable>> = Vec::new();
   list.push(Box::new(Sphere::new(Vec3::new(0.0,0.0,-1.0), 0.5)));
   list.push(Box::new(Sphere::new(Vec3::new(0.0,-100.5,-1.0), 100.0)));
-
   let world = Hitable_list::new(list);
+
+  let mut rng = rand::thread_rng();
 
   for j in (0..ny).rev() {
     for i in 0..nx {
-      let u = i as f64 / nx as f64;
-      let v = j as f64 / ny as f64;
-      let r = Ray::new(origin, lower_left_corner + horizontal * u + vertical * v);
+      let mut col = Vec3::origin();
+      for s in 0..ns {
+        let u = (i as f64 + rng.gen::<f64>()) / nx as f64;
+        let v = (j as f64 + rng.gen::<f64>()) / ny as f64;
+        let r = cam.get_ray(u, v);
 
-      let p = r.point_at_parameter(2.0);
-      let col = color(r, &world);
+        let p = r.point_at_parameter(2.0);
+        col += color(r, &world);
+      }
+
+      col /= ns as f64;
 
       let ir = (255.99*col.r()) as i32;
       let ig = (255.99*col.g()) as i32;
