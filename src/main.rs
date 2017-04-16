dbb6bb23fa2990f43b14c2200cec86cdbc592131
Thomas Y <corragon@gmail.com>
2017-04-16 15:03:08 -0500

    Refactor pixel calculation into separate function

--- src/main.rs
+++ src/main.rs
@@ -31,10 +31,25 @@ fn color<T: Hitable>(ray : Ray, world : &T) -> Vec3 {
   }
 }
 
+fn random<T: Hitable>(i : i32, j : i32, nx : i32, ny : i32, cam : Camera, world : &T, rng : &mut rand::ThreadRng) -> Vec3 {
+  let ns = 100;
+  let mut col = Vec3::origin();
+  for s in 0..ns {
+    let u = (i as f64 + rng.gen::<f64>()) / nx as f64;
+    let v = (j as f64 + rng.gen::<f64>()) / ny as f64;
+    let r = cam.get_ray(u, v);
+
+    let p = r.point_at_parameter(2.0);
+    col += color(r, world);
+  }
+
+  col /= ns as f64;
+  return col;
+}
+
 fn main() {
   let nx = 200;
   let ny = 100;
-  let ns = 100;
   let data = format!("{}\n{} {}\n{}\n", "P3", nx, ny, 255);
   let f = File::create("target/image.ppm").expect("Unable to create file");
   let mut f = BufWriter::new(f);
@@ -56,17 +71,7 @@ fn main() {
 
   for j in (0..ny).rev() {
     for i in 0..nx {
-      let mut col = Vec3::origin();
-      for s in 0..ns {
-        let u = (i as f64 + rng.gen::<f64>()) / nx as f64;
-        let v = (j as f64 + rng.gen::<f64>()) / ny as f64;
-        let r = cam.get_ray(u, v);
-
-        let p = r.point_at_parameter(2.0);
-        col += color(r, &world);
-      }
-
-      col /= ns as f64;
+      let col = random(i, j, nx, ny, cam, &world, &mut rng);
 
       let ir = (255.99*col.r()) as i32;
       let ig = (255.99*col.g()) as i32;
