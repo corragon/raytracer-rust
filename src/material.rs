295d2d4cb983a0965e906ce048bd9e3197d2f64c
Thomas Y <corragon@gmail.com>
2017-05-04 22:57:10 -0500

    Change arguments to be mutable

--- src/material.rs
+++ src/material.rs
@@ -3,5 +3,5 @@ use vector::Vec3;
 use hitable::HitRecord;
 
 pub trait Material {
-  fn scatter(&self, r: &Ray, rec: &HitRecord, attenuation: &Vec3, scattered: &Ray) -> bool;
+  fn scatter(&self, r: &Ray, rec: &HitRecord, attenuation: &mut Vec3, scattered: &mut Ray) -> bool;
 }
